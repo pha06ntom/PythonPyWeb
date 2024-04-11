2c211f3dd8f458e2430c1d877ae78516f5b666f6
arispi <pha06ntom@mail.ru>
2024-04-11 21:05:38 +0300

    Laba3_(start: point 1.3.1 requests)

--- apps/db_train_alternative/views.py
+++ apps/db_train_alternative/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from .models import Author
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+
+class AuthorREST(View):
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get(self, request, id=None):
+
+        if id is None:  # Проверяем, что требуется вернуть всех пользователей
+            data = []
+            for author in Author.objects.all():
+                # Производим сериализацию, т.е. определяем, что именно запишется в данные для преобразования в JSON
+                data_author = {'id': author.id,
+                               'name': author.name,
+                               'email': author.email}
+                data.append(data_author)
+        else:
+            author = Author.objects.filter(id=id)
+            if author:  # Если автор такой есть, т.е. QuerySet не пустой
+                author = author.first()  # Получаем первого автора из QuerySet, так как он там один
+                # Производим сериализацию, т.е. определяем, что именно запишется в данные для преобразования в JSON
+                data = {'id': author.id,
+                        'name': author.name,
+                        'email': author.email}
+            else:  # Иначе, так как автор не найден (QuerySet пустой), то возвращаем ошибку, с произвольным текстом,
+                # для понимания почему произошла ошибка
+                return JsonResponse({'error': f'Автор с id={id} не найден!'},
+                                    status=404,
+                                    json_dumps_params={"ensure_ascii": False,
+                                                       "indent": 4}
+                                    )
+
+        # После того как данные для ответа созданы - возвращаем Json объект с данными
+        return JsonResponse(data, safe=False, json_dumps_params={"ensure_ascii": False,
+                                                                 "indent": 4})
+
+    def post(self, request):
+        # Создание нвого автора в REST стиле
+        try:
+            data = json.loads(request.body)
+
+            author = Author(name=data['name'], email=data['email'])
+            author.clean_fields() # Запуск валидаций
+            author.save() # Сохранение в БД
+
+            response_data = {
+                'message': f'Автор успешно создан',
+                'id': author.id,
+                'name': author.name,
+                'email': author.email
+            }
+
+            return JsonResponse(response_data, status=201,
+                                json_dumps_params={'ensure_ascii': False,
+                                                   'indent': 4}
+                                )
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400,
+                                json_dumps_params={'ensure_ascii': False,
+                                                   'indent':4}
+                                )
+
+    def put(self, request, id):
+        try:
+            author = Author.objects.get(id=id)
+            data = json.loads(request.body)
+            # Обновляем поля
+            author.name = data['name']
+            author.email = data['email']
+            author.clean_fields() # Запуск валидаций
+            author.save() # Сохранение в БД
+
+            response_data = {
+                'message': f'Данные автора успешно изменены',
+                'id': author.id,
+                'name': author.name,
+                'email': author.email
+            }
+
+            return JsonResponse(response_data,
+                                json_dumps_params={'ensure_ascii': False,
+                                                   'indent': 4},
+                                )
+        except Author.DoesNotExist: # Если получили ошибку
+            return JsonResponse({'error': 'Автор не найден'},
+                                status=404,
+                                json_dumps_params={"ensure_ascii": False,
+                                                   "indent": 4},
+                                )
+        except Exception as e: # При любой другой ошибке
+            return JsonResponse({'error': str(e)},
+                                status=400,
+                                json_dumps_params={'ensure_ascii': False,
+                                                   'indent': 4},
+                                )
