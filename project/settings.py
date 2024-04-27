8ef86a29a5220f0190d9da488f24d6b47e87f743
Arina <pha06ntom@mail.ru>
2024-04-27 18:53:23 +0300

    lab_5 (correction db)

--- project/settings.py
+++ project/settings.py
@@ -109,14 +109,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME_PGDB'),
-        'USER': os.getenv('USER_PGDB'),
-        'PASSWORD': os.getenv('PASSWORD_PGDB'),
-        'HOST': os.getenv('HOST_PGDB'),
-        'PORT': os.getenv('POST_PGDB'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('NAME_PGDB'),
+    #     'USER': os.getenv('USER_PGDB'),
+    #     'PASSWORD': os.getenv('PASSWORD_PGDB'),
+    #     'HOST': os.getenv('HOST_PGDB'),
+    #     'PORT': os.getenv('POST_PGDB'),
+    # }
 }
 
 
